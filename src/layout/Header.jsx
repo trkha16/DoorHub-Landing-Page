3a9c38bff58cc2aa2eab1573b4a8e428a779a695
trkha16 <tbkha1609@gmail.com>
2022-09-01 02:57:14 +0700

    responsive header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,17 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import logo from "../assets/logo.png";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 const HeaderStyles = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     padding: 24px 8%;
     ul {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 40px;
         list-style: none;
         li {
             line-height: 16px;
@@ -24,29 +19,115 @@ const HeaderStyles = styled.div`
             }
         }
     }
-    .button {
-        background: linear-gradient(153.09deg, #5390ff 10.28%, #1660e6 102.17%);
-        box-shadow: 0px 16px 40px -8px rgba(56, 126, 250, 0.28);
-        border-radius: 40px;
-        padding: 14px 36px;
-        font-weight: 6000;
-        line-height: 16px;
-        text-align: center;
-        color: white;
-        cursor: pointer;
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        ul {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 40px;
+            list-style: none;
+            li {
+                line-height: 16px;
+                font-weight: 500;
+                font-style: normal;
+                color: #aaa7b1;
+                &:hover {
+                    color: #2e2c49;
+                    cursor: pointer;
+                }
+            }
+        }
+        .button {
+            background: linear-gradient(
+                153.09deg,
+                #5390ff 10.28%,
+                #1660e6 102.17%
+            );
+            box-shadow: 0px 16px 40px -8px rgba(56, 126, 250, 0.28);
+            border-radius: 40px;
+            padding: 14px 36px;
+            font-weight: 6000;
+            line-height: 16px;
+            text-align: center;
+            color: white;
+            cursor: pointer;
+        }
+    }
+
+    .navbar-hide {
+        display: none;
+    }
+
+    @media screen and (max-width: 1060px) {
+        .navbar {
+            display: none;
+        }
+        .navbar-hide {
+            display: flex;
+            justify-content: space-between;
+            .menu-container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                position: absolute;
+                right: 1%;
+                background: black;
+                border-radius: 7px;
+                li {
+                    padding: 16px 0;
+                }
+                .button {
+                    padding: 14px 36px;
+                    line-height: 16px;
+                    font-weight: 500;
+                    font-style: normal;
+                    color: #aaa7b1;
+                }
+            }
+        }
     }
 `;
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <HeaderStyles>
-            <img src={logo} alt="Logo" />
-            <ul>
-                {navLinks.map((nav) => (
-                    <li key={nav.id}>{nav.title}</li>
-                ))}
-            </ul>
-            <div className="button">Contact Us</div>
+            <div className="navbar">
+                <img src={logo} alt="Logo" className="logo" />
+                <ul>
+                    {navLinks.map((nav) => (
+                        <li key={nav.id}>{nav.title}</li>
+                    ))}
+                </ul>
+                <div className="button">Contact Us</div>
+            </div>
+
+            <div className="navbar-hide">
+                <img src={logo} alt="Logo" className="logo" />
+
+                <div>
+                    <div onClick={() => setToggle((prev) => !prev)}>
+                        {toggle ? <CloseIcon /> : <MenuIcon />}
+                    </div>
+                    {toggle ? (
+                        <div className="menu-container">
+                            <ul>
+                                {navLinks.map((nav) => (
+                                    <li key={nav.id}>{nav.title}</li>
+                                ))}
+                            </ul>
+                            <div className="button">Contact Us</div>
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                </div>
+            </div>
         </HeaderStyles>
     );
 };
